1db582a6c4ce6cac913f4e1a6a30aae5fa6eef19
OutOfCache <outofcache@tutanota.com>
2020-08-08 17:29:32 +0200

    Made Read and Write functions visible outside

--- chip8/memory.go
+++ chip8/memory.go
@@ -4,10 +4,10 @@ type Memory struct {
     var mem [4096]byte
 }
 
-func (mem Memory) read(adr uint16) byte {
+func (mem Memory) Read(adr uint16) byte {
     return mem[adr]
 }
 
-func (mem Memory) write(adr uint16, val byte) {
+func (mem Memory) Write(adr uint16, val byte) {
     mem[adr] = val
 }
