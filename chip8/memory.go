2cc1f749be0194fbaa0b523b448beab81b4b4bc4
OutOfCache <outofcache@tutanota.com>
2020-08-16 14:08:10 +0200

    Added tests for memory

--- chip8/memory.go
+++ chip8/memory.go
@@ -1,13 +1,13 @@
 package chip8
 
 type Memory struct {
-    var mem [4096]byte
+    Memory [4096]byte
 }
 
-func (mem Memory) Read(adr uint16) byte {
-    return mem[adr]
+func (mem *Memory) Read(adr uint16) byte {
+    return mem.Memory[adr]
 }
 
-func (mem Memory) Write(adr uint16, val byte) {
-    mem[adr] = val
+func (mem *Memory) Write(adr uint16, val byte) {
+    mem.Memory[adr] = val
 }
